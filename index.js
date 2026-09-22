07ed2f455a12b3c696ba8ddb8773d80eb6393096
bmk24 <71297305+brightluke@users.noreply.github.com>
2026-09-22 18:05:34 +0200

    feat: export live reload API

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 
 const { buildProject } = require("./src/build-project");
 const { run } = require("./src/cli");
-const { createProject, validateProjectName } = require("./src/create-project");
+const {
+  createProject,
+  validateProjectName
+} = require("./src/create-project");
 const { startDevServer } = require("./src/dev-server");
+const {
+  createLiveReload,
+  injectLiveReload
+} = require("./src/live-reload");
 const { loadProject } = require("./src/project");
 
 module.exports = {
   buildProject,
+  createLiveReload,
   createProject,
+  injectLiveReload,
   loadProject,
   run,
   startDevServer,

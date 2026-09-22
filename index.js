5785690a3eff8017dce38988ecdfa3fae7521441
bmk24 <71297305+brightluke@users.noreply.github.com>
2026-09-22 16:51:33 +0200

    feat: wire MANGANI v0.2 CLI

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 "use strict";
 
+const { buildProject } = require("./src/build-project");
 const { run } = require("./src/cli");
 const { createProject, validateProjectName } = require("./src/create-project");
+const { startDevServer } = require("./src/dev-server");
+const { loadProject } = require("./src/project");
 
 module.exports = {
+  buildProject,
   createProject,
+  loadProject,
   run,
+  startDevServer,
   validateProjectName
 };
